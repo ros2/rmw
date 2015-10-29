2c6ffdb1fae244f6a0fa0b85921b05019afb93c8
William Woodall <william@osrfoundation.org>
2015-10-29 02:03:50 -0700

    implement work around for Windows
    This is needed because Windows' linker does
    not support unresolved symbols when linking.

--- rmw/rmw-extras.cmake
+++ rmw/rmw-extras.cmake
@@ -17,3 +17,34 @@
 include("${rmw_DIR}/configure_rmw_library.cmake")
 include("${rmw_DIR}/get_rmw_typesupport.cmake")
 include("${rmw_DIR}/register_rmw_implementation.cmake")
+
+# TODO(wjwwood): we have to fix linking with unresolved symbols on Windows before removing this.
+if(WIN32)
+  find_package(ament_cmake REQUIRED)  # Without this find_package(rmw_implementation_cmake) fails.
+  find_package(rmw_implementation_cmake REQUIRED)
+  get_available_rmw_implementations(_rmw_implementations)
+  list(LENGTH _rmw_implementations _len)
+  # If there are no middleware yet, then continue.
+  # This happends when the first middleware implementation is built.
+  if(${_len} EQUAL 0)
+    set(_rmw_implementations)
+    set(_len)
+    return()
+  endif()
+  # Ensure only one middleware is available.
+  # If more than one is available error out in order to prevent other packages from trying
+  # to build libraries and executables for more than one rmw implementation at a time.
+  if(${_len} GREATER 1)
+    message(FATAL_ERROR
+      "Only one rmw implementation per workspace is supported on Windows, but ${_len} were found."
+    )
+  endif()
+  set(_rmw_implementations)
+  set(_len)
+  # Use get_default_rmw_implementation to export the default middleware as a transitive dependency.
+  get_default_rmw_implementation(middleware_implementation)
+  find_package(${middleware_implementation} REQUIRED)
+  list(APPEND rmw_DEFINITIONS ${${middleware_implementation}_DEFINITIONS})
+  list(APPEND rmw_INCLUDE_DIRS ${${middleware_implementation}_INCLUDE_DIRS})
+  list(APPEND rmw_LIBRARIES ${${middleware_implementation}_LIBRARIES})
+endif()
