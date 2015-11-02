20c06d348a67ba44df43f618f8286774553954b8
William Woodall <william@osrfoundation.org>
2015-11-02 12:11:15 -0800

    address linting warning in cmake file

--- rmw_implementation_cmake/rmw_implementation_cmake-extras.cmake
+++ rmw_implementation_cmake/rmw_implementation_cmake-extras.cmake
@@ -14,6 +14,8 @@
 
 # copied from rmw_implementation_cmake/rmw_implementation_cmake-extras.cmake
 
-include("${rmw_implementation_cmake_DIR}/add_executable_for_each_rmw_implementation.cmake")
+include(
+  "${rmw_implementation_cmake_DIR}/add_executable_for_each_rmw_implementation.cmake"
+)
 include("${rmw_implementation_cmake_DIR}/get_available_rmw_implementations.cmake")
 include("${rmw_implementation_cmake_DIR}/get_default_rmw_implementation.cmake")
