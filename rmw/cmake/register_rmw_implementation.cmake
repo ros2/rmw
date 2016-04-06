f7986947a74d792af27ea7bc55e75e7893ab7a4d
Jackie Kay <jackie@osrfoundation.org>
2016-04-06 11:36:20 -0700

    Add macros for getting C or C++ typ support for an rmw implementation

--- rmw/cmake/register_rmw_implementation.cmake
+++ rmw/cmake/register_rmw_implementation.cmake
@@ -14,18 +14,40 @@
 
 #
 # Register the current package as a ROS middleware implementation.
+# register_rmw_implementation must be called with one or more arguments.
+# The arguments each must be specified with flags corresonding to their type
+# e.g. C specifies a C typesupport, CPP specifies a C++ typesupport.
 #
-# :param typesupport_package_name: the package name providing type support for
-#   for the registered RMW implementation
-# :type typesupport_package_name: string
+# :param _ARG_C: Name of the C typesupport package for the rmw implementation.
+# :param _ARG_CPP: Name of the C++ typesupport package.
+# :type _ARG_C: string
+# :type _ARG_CPP: string
 #
 # @public
 #
-macro(register_rmw_implementation typesupport_package_name)
-  if(NOT "${ARGN} " STREQUAL " ")
-    message(FATAL_ERROR "register_rmw_implementation() called with "
-      "unused arguments: ${ARGN}")
+macro(register_rmw_implementation)
+  if("${ARGN} " STREQUAL " ")
+    message(FATAL_ERROR "register_rmw_implementation() called with no arguments!")
   endif()
 
-  ament_index_register_resource("rmw_implementation" CONTENT "${typesupport_package_name}")
+  cmake_parse_arguments(_ARG
+    "TRACE"
+    ""
+    "C;CPP"
+    ${ARGN}
+  )
+  if (_ARG_UNPARSED_ARGUMENTS)
+    message(FATAL_ERROR
+      "register_rmw_implementation() called with unused arguments: ${_ARG_UNPARSED_ARGUMENTS}")
+  endif()
+  if (_ARG_C)
+    ament_index_register_resource("rmw_typesupport_c" CONTENT "${_ARG_C}")
+    ament_index_register_resource("rmw_typesupport" CONTENT "${_ARG_C}")
+  endif()
+
+  if (_ARG_CPP)
+    ament_index_register_resource("rmw_typesupport_cpp" CONTENT "${_ARG_CPP}")
+    ament_index_register_resource("rmw_typesupport" CONTENT "${_ARG_CPP}")
+  endif()
+
 endmacro()
