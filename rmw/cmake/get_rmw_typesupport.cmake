618c512a11e775bc58580c5fea6db8f5444da9b1
Jackie Kay <jackie@osrfoundation.org>
2016-04-13 14:56:29 -0700

    Consolidate into one function with LANGUAGE arg

--- rmw/cmake/get_rmw_typesupport.cmake
+++ rmw/cmake/get_rmw_typesupport.cmake
@@ -18,47 +18,23 @@
 # :param rmw_implementation: the package name of the ROS middleware
 #   implementation
 # :type rmw_implementation: string
+# :param language: Optional flag specifying the language of the type support to get.
 #
 # @public
 #
 macro(get_rmw_typesupport var rmw_implementation)
-  if(NOT "${ARGN} " STREQUAL " ")
-    message(FATAL_ERROR "get_rmw_typesupport() called with unused arguments: ${ARGN}")
+  cmake_parse_arguments(_ARG
+    ""
+    "LANGUAGE"
+    ""
+    ${ARGN}
+  )
+  if("${_ARG_LANGUAGE}" STREQUAL "C")
+    ament_index_get_resource(${var} "rmw_typesupport_c" ${rmw_implementation})
+  elseif("${_ARG_LANGUAGE}" STREQUAL "CPP")
+    ament_index_get_resource(${var} "rmw_typesupport_cpp" ${rmw_implementation})
+  else()
+    # This output will include type supports for all languages.
+    ament_index_get_resource(${var} "rmw_typesupport" ${rmw_implementation})
   endif()
-
-  ament_index_get_resource(${var} "rmw_typesupport" ${rmw_implementation})
-endmacro()
-
-#
-# Get the C type support package name(s) for a specific ROS middleware implementation.
-#
-# :param rmw_implementation: the package name of the ROS middleware
-#   implementation
-# :type rmw_implementation: string
-#
-# @public
-#
-macro(get_rmw_typesupport_c var rmw_implementation)
-  if(NOT "${ARGN} " STREQUAL " ")
-    message(FATAL_ERROR "get_rmw_typesupport_c() called with unused arguments: ${ARGN}")
-  endif()
-
-  ament_index_get_resource(${var} "rmw_typesupport_c" "${rmw_implementation}")
-endmacro()
-
-#
-# Get the C++ type support package name(s) for a specific ROS middleware implementation.
-#
-# :param rmw_implementation: the package name of the ROS middleware
-#   implementation
-# :type rmw_implementation: string
-#
-# @public
-#
-macro(get_rmw_typesupport_cpp var rmw_implementation)
-  if(NOT "${ARGN} " STREQUAL " ")
-    message(FATAL_ERROR "get_rmw_typesupport_cpp() called with unused arguments: ${ARGN}")
-  endif()
-
-  ament_index_get_resource(${var} "rmw_typesupport_cpp" "${rmw_implementation}")
 endmacro()
