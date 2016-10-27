02e1d77a31701f76f18967bb1bb3cca8bc6e7a31
Mikael Arguedas <mikael.arguedas@gmail.com>
2016-10-27 15:30:17 -0700

    include order

--- rmw/src/allocators.c
+++ rmw/src/allocators.c
@@ -12,10 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "rmw/allocators.h"
+
 #include <stdlib.h>
 #include <string.h>
 
-#include "rmw/allocators.h"
 #include "rmw/types.h"
 
 void *
