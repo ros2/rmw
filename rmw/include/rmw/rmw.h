987da30b59dc1a043c8d13269fea02a5b01c967e
Mikael Arguedas <mikael.arguedas@gmail.com>
2016-12-14 09:59:02 -0800

    interface for secure node

--- rmw/include/rmw/rmw.h
+++ rmw/include/rmw/rmw.h
@@ -94,6 +94,11 @@ rmw_create_node(const char * name, const char * namespace_, size_t domain_id);
 
 RMW_PUBLIC
 RMW_WARN_UNUSED
+rmw_node_t *
+rmw_create_secure_node(const char * name, size_t domain_id, char ** security_files_paths);
+
+RMW_PUBLIC
+RMW_WARN_UNUSED
 rmw_ret_t
 rmw_destroy_node(rmw_node_t * node);
 
