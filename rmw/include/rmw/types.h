0ebcc3eedea601b3a76e5d70f0a9e6c376327464
William Woodall <william@osrfoundation.org>
2017-03-29 17:17:45 -0700

    name_space -> namespace_

--- rmw/include/rmw/types.h
+++ rmw/include/rmw/types.h
@@ -45,7 +45,7 @@ typedef struct RMW_PUBLIC_TYPE rmw_node_t
   const char * implementation_identifier;
   void * data;
   const char * name;
-  const char * name_space;
+  const char * namespace_;
   const size_t domain_id;
 } rmw_node_t;
 
