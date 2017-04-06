383db0f1e50133cf4fb17d66add44d4db8ef2887
William Woodall <william@osrfoundation.org>
2017-04-06 16:53:32 -0700

    add rmw_validate_namespace()

---
+++ rmw/src/validate_namespace.c
@@ -0,0 +1,123 @@
+// Copyright 2017 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "rmw/validate_namespace.h"
+
+#include <ctype.h>
+#include <string.h>
+
+#include "rmw/error_handling.h"
+#include "rmw/validate_topic_name.h"
+
+#include "./isalnum_no_locale.h"
+
+rmw_ret_t
+rmw_validate_namespace(
+  const char * namespace_,
+  int * validation_result,
+  size_t * invalid_index)
+{
+  if (!namespace_) {
+    return RMW_RET_INVALID_ARGUMENT;
+  }
+  if (!validation_result) {
+    return RMW_RET_INVALID_ARGUMENT;
+  }
+  if (!invalid_index) {
+    return RMW_RET_INVALID_ARGUMENT;
+  }
+
+  size_t namespace_length = strlen(namespace_);
+  // Special case for root namepsace
+  if (namespace_length == 1 && namespace_[0] == '/') {
+    // Ok to return here, it is valid and will not exceed RMW_NAMESPACE_MAX_LENGTH.
+    *validation_result = RMW_NAMESPACE_VALID;
+    return RMW_RET_OK;
+  }
+
+  // All other cases should pass the validate topic name test.
+  int t_validation_result;
+  size_t t_invalid_index;
+  rmw_ret_t ret = rmw_validate_topic_name(namespace_, &t_validation_result, &t_invalid_index);
+  if (ret != RMW_RET_OK) {
+    // error already set
+    return ret;
+  }
+
+  if (t_validation_result != RMW_TOPIC_VALID && t_validation_result != RMW_TOPIC_INVALID_TOO_LONG) {
+    switch (t_validation_result) {
+      case RMW_TOPIC_INVALID_IS_EMPTY_STRING:
+        *validation_result = RMW_NAMESPACE_INVALID_IS_EMPTY_STRING;
+        break;
+      case RMW_TOPIC_INVALID_NOT_ABSOLUTE:
+        *validation_result = RMW_NAMESPACE_INVALID_NOT_ABSOLUTE;
+        break;
+      case RMW_TOPIC_INVALID_ENDS_WITH_FORWARD_SLASH:
+        *validation_result = RMW_NAMESPACE_INVALID_ENDS_WITH_FORWARD_SLASH;
+        break;
+      case RMW_TOPIC_INVALID_CONTAINS_UNALLOWED_CHARACTERS:
+        *validation_result = RMW_NAMESPACE_INVALID_CONTAINS_UNALLOWED_CHARACTERS;
+        break;
+      case RMW_TOPIC_INVALID_CONTAINS_REPEATED_FORWARD_SLASH:
+        *validation_result = RMW_NAMESPACE_INVALID_CONTAINS_REPEATED_FORWARD_SLASH;
+        break;
+      case RMW_TOPIC_INVALID_NAME_TOKEN_STARTS_WITH_NUMBER:
+        *validation_result = RMW_NAMESPACE_INVALID_NAME_TOKEN_STARTS_WITH_NUMBER;
+        break;
+      default:
+        RMW_SET_ERROR_MSG(
+          "rmw_validate_namespace(): unknown rmw_validate_topic_name() validation result"
+        );
+        return RMW_RET_ERROR;
+    }
+    *invalid_index = t_invalid_index;
+    return RMW_RET_OK;
+  }
+
+  // check if the namespace is too long last, since it might be a soft invalidation
+  if (namespace_length > RMW_NAMESPACE_MAX_LENGTH) {
+    *validation_result = RMW_NAMESPACE_INVALID_TOO_LONG;
+    *invalid_index = RMW_NAMESPACE_MAX_LENGTH - 1;
+    return RMW_RET_OK;
+  }
+
+  // everything was ok, set result to valid namespace, avoid setting invalid_index, and return
+  *validation_result = RMW_NAMESPACE_VALID;
+  return RMW_RET_OK;
+}
+
+const char *
+rmw_namespace_validation_result_string(int validation_result)
+{
+  switch (validation_result) {
+    case RMW_NAMESPACE_VALID:
+      return NULL;
+    case RMW_NAMESPACE_INVALID_IS_EMPTY_STRING:
+      return "namespace must not be empty";
+    case RMW_NAMESPACE_INVALID_NOT_ABSOLUTE:
+      return "namespace must be absolute, it must lead with a '/'";
+    case RMW_NAMESPACE_INVALID_ENDS_WITH_FORWARD_SLASH:
+      return "namespace must not end with a '/', unless only a '/'";
+    case RMW_NAMESPACE_INVALID_CONTAINS_UNALLOWED_CHARACTERS:
+      return "namespace must not contain characters other than alphanumerics, '_', or '/'";
+    case RMW_NAMESPACE_INVALID_CONTAINS_REPEATED_FORWARD_SLASH:
+      return "namespace must not contain repeated '/'";
+    case RMW_NAMESPACE_INVALID_NAME_TOKEN_STARTS_WITH_NUMBER:
+      return "namespace must not have a token that starts with a number";
+    case RMW_NAMESPACE_INVALID_TOO_LONG:
+      return "namespace should not exceed '" RMW_STRINGIFY(RMW_NAMESPACE_MAX_NAME_LENGTH) "'";
+    default:
+      return NULL;
+  }
+}
---
+++ rmw/test/test_validate_namespace.cpp
@@ -0,0 +1,178 @@
+// Copyright 2017 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+
+#include "gmock/gmock.h"
+
+#include "rmw/validate_namespace.h"
+
+TEST(test_validate_namespace, invalid_parameters) {
+  int validation_result;
+  size_t invalid_index;
+  rmw_ret_t ret = rmw_validate_namespace(nullptr, &validation_result, &invalid_index);
+  ASSERT_EQ(RMW_RET_INVALID_ARGUMENT, ret);
+  ret = rmw_validate_namespace("/test", nullptr, &invalid_index);
+  ASSERT_EQ(RMW_RET_INVALID_ARGUMENT, ret);
+  ret = rmw_validate_namespace("/test", &validation_result, nullptr);
+  ASSERT_EQ(RMW_RET_INVALID_ARGUMENT, ret);
+}
+
+TEST(test_validate_namespace, valid_namespace) {
+  int validation_result;
+  size_t invalid_index;
+  rmw_ret_t ret;
+
+  ret = rmw_validate_namespace("/", &validation_result, &invalid_index);
+  ASSERT_EQ(RMW_RET_OK, ret);
+  ASSERT_EQ(RMW_NAMESPACE_VALID, validation_result);
+
+  validation_result = -1;
+  ret = rmw_validate_namespace("/basename_only", &validation_result, &invalid_index);
+  ASSERT_EQ(RMW_RET_OK, ret);
+  ASSERT_EQ(RMW_NAMESPACE_VALID, validation_result);
+
+  validation_result = -1;
+  ret = rmw_validate_namespace("/with_one/heirarchy", &validation_result, &invalid_index);
+  ASSERT_EQ(RMW_RET_OK, ret);
+  ASSERT_EQ(RMW_NAMESPACE_VALID, validation_result);
+
+  ASSERT_EQ((char *)NULL, rmw_topic_validation_result_string(validation_result));
+}
+
+TEST(test_validate_namespace, empty_namespace) {
+  int validation_result;
+  size_t invalid_index;
+  rmw_ret_t ret;
+
+  ret = rmw_validate_namespace("", &validation_result, &invalid_index);
+  ASSERT_EQ(RMW_RET_OK, ret);
+  ASSERT_EQ(RMW_NAMESPACE_INVALID_IS_EMPTY_STRING, validation_result);
+  ASSERT_EQ(0ul, invalid_index);
+
+  ASSERT_NE((char *)NULL, rmw_topic_validation_result_string(validation_result));
+}
+
+TEST(test_validate_namespace, not_absolute) {
+  int validation_result;
+  size_t invalid_index;
+  rmw_ret_t ret;
+
+  ret = rmw_validate_namespace("not_absolute", &validation_result, &invalid_index);
+  ASSERT_EQ(RMW_RET_OK, ret);
+  ASSERT_EQ(RMW_NAMESPACE_INVALID_NOT_ABSOLUTE, validation_result);
+  ASSERT_EQ(0ul, invalid_index);
+
+  ret = rmw_validate_namespace("not/absolute", &validation_result, &invalid_index);
+  ASSERT_EQ(RMW_RET_OK, ret);
+  ASSERT_EQ(RMW_NAMESPACE_INVALID_NOT_ABSOLUTE, validation_result);
+  ASSERT_EQ(0ul, invalid_index);
+
+  ASSERT_NE((char *)NULL, rmw_topic_validation_result_string(validation_result));
+}
+
+TEST(test_validate_namespace, ends_with_forward_slash) {
+  int validation_result;
+  size_t invalid_index;
+  rmw_ret_t ret;
+
+  ret = rmw_validate_namespace("/ends/with/", &validation_result, &invalid_index);
+  ASSERT_EQ(RMW_RET_OK, ret);
+  ASSERT_EQ(RMW_NAMESPACE_INVALID_ENDS_WITH_FORWARD_SLASH, validation_result);
+  ASSERT_EQ(10ul, invalid_index);
+
+  ASSERT_NE((char *)NULL, rmw_topic_validation_result_string(validation_result));
+}
+
+TEST(test_validate_namespace, unallowed_characters) {
+  int validation_result;
+  size_t invalid_index;
+  rmw_ret_t ret;
+
+  ret = rmw_validate_namespace("/~/unexpanded_tilde", &validation_result, &invalid_index);
+  ASSERT_EQ(RMW_RET_OK, ret);
+  ASSERT_EQ(RMW_NAMESPACE_INVALID_CONTAINS_UNALLOWED_CHARACTERS, validation_result);
+  ASSERT_EQ(1ul, invalid_index);
+
+  ret = rmw_validate_namespace("/unexpanded_sub/{node}", &validation_result, &invalid_index);
+  ASSERT_EQ(RMW_RET_OK, ret);
+  ASSERT_EQ(RMW_NAMESPACE_INVALID_CONTAINS_UNALLOWED_CHARACTERS, validation_result);
+  ASSERT_EQ(16ul, invalid_index);
+
+  ret = rmw_validate_namespace("/question?", &validation_result, &invalid_index);
+  ASSERT_EQ(RMW_RET_OK, ret);
+  ASSERT_EQ(RMW_NAMESPACE_INVALID_CONTAINS_UNALLOWED_CHARACTERS, validation_result);
+  ASSERT_EQ(9ul, invalid_index);
+
+  ret = rmw_validate_namespace("/with spaces", &validation_result, &invalid_index);
+  ASSERT_EQ(RMW_RET_OK, ret);
+  ASSERT_EQ(RMW_NAMESPACE_INVALID_CONTAINS_UNALLOWED_CHARACTERS, validation_result);
+  ASSERT_EQ(5ul, invalid_index);
+
+  ASSERT_NE((char *)NULL, rmw_topic_validation_result_string(validation_result));
+}
+
+TEST(test_validate_namespace, repeated_forward_slashes) {
+  int validation_result;
+  size_t invalid_index;
+  rmw_ret_t ret;
+
+  ret = rmw_validate_namespace("/repeated//slashes", &validation_result, &invalid_index);
+  ASSERT_EQ(RMW_RET_OK, ret);
+  ASSERT_EQ(RMW_NAMESPACE_INVALID_CONTAINS_REPEATED_FORWARD_SLASH, validation_result);
+  ASSERT_EQ(10ul, invalid_index);
+
+  ASSERT_NE((char *)NULL, rmw_topic_validation_result_string(validation_result));
+}
+
+TEST(test_validate_namespace, starts_with_number) {
+  int validation_result;
+  size_t invalid_index;
+  rmw_ret_t ret;
+
+  ret = rmw_validate_namespace("/9starts_with_number", &validation_result, &invalid_index);
+  ASSERT_EQ(RMW_RET_OK, ret);
+  ASSERT_EQ(RMW_NAMESPACE_INVALID_NAME_TOKEN_STARTS_WITH_NUMBER, validation_result);
+  ASSERT_EQ(1ul, invalid_index);
+
+  ret = rmw_validate_namespace("/starts/42with/number", &validation_result, &invalid_index);
+  ASSERT_EQ(RMW_RET_OK, ret);
+  ASSERT_EQ(RMW_NAMESPACE_INVALID_NAME_TOKEN_STARTS_WITH_NUMBER, validation_result);
+  ASSERT_EQ(8ul, invalid_index);
+
+  ASSERT_NE((char *)NULL, rmw_topic_validation_result_string(validation_result));
+}
+
+TEST(test_validate_namespace, topic_too_long) {
+  int validation_result;
+  size_t invalid_index;
+  rmw_ret_t ret;
+
+  // Ensure the length is not the first error
+  std::string invalid_and_long_topic(RMW_NAMESPACE_MAX_LENGTH + 1, 'a');
+  ret = rmw_validate_namespace(
+    invalid_and_long_topic.c_str(), &validation_result, &invalid_index);
+  ASSERT_EQ(RMW_RET_OK, ret);
+  ASSERT_EQ(RMW_NAMESPACE_INVALID_NOT_ABSOLUTE, validation_result);
+  ASSERT_EQ(0ul, invalid_index);
+
+  std::string valid_but_long_topic = "/" + invalid_and_long_topic;
+  ret = rmw_validate_namespace(
+    valid_but_long_topic.c_str(), &validation_result, &invalid_index);
+  ASSERT_EQ(RMW_RET_OK, ret);
+  EXPECT_EQ(RMW_NAMESPACE_INVALID_TOO_LONG, validation_result);
+  EXPECT_EQ(RMW_NAMESPACE_MAX_LENGTH - 1, invalid_index);
+
+  ASSERT_NE((char *)NULL, rmw_topic_validation_result_string(validation_result));
+}
