3121262542023d1779d621877d2143b7e4e5efcd
Esteve Fernandez <esteve@apache.org>
2017-04-13 04:11:04 +0200

    Add support for pthread-based thread local storage (#80)
    * Added support for pthread-based thread local storage (e.g. for iOS < 10.0)
    
    * Readd SHARED to add_library until ROS2 can be built statically via ament_cmake_ros
    
    * change detection of apple OS's
    
    also undef RMW_THREAD_LOCAL to cause compiler
    error in the case that pthread must be used
    
    * line length fix
    
    * remove SHARED again now that #98 has been merged

--- rmw/include/rmw/macros.h
+++ rmw/include/rmw/macros.h
@@ -15,10 +15,37 @@
 #ifndef RMW__MACROS_H_
 #define RMW__MACROS_H_
 
+// This block either sets RMW_THREAD_LOCAL or RMW_THREAD_LOCAL_PTHREAD.
 #if defined _WIN32 || defined __CYGWIN__
+// Windows or Cygwin
   #define RMW_THREAD_LOCAL __declspec(thread)
+#elif defined __APPLE__
+// Apple OS's
+  #include <TargetConditionals.h>
+  #if TARGET_IPHONE_SIMULATOR || TARGET_OS_IPHONE
+// iOS Simulator or iOS device
+    #include <Availability.h>
+    #if defined(__IPHONE_OS_VERSION_MIN_REQUIRED)
+      #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 100000
+// iOS >= 10, thread local storage was added in iOS 10
+        #define RMW_THREAD_LOCAL _Thread_local
+      #else
+// iOS < 10, no thread local storage, so use pthread instead
+        #define RMW_THREAD_LOCAL_PTHREAD 1
+        #undef RMW_THREAD_LOCAL
+      #endif
+    #else
+      #error "Unknown iOS version"
+    #endif
+  #elif TARGET_OS_MAC
+// macOS
+    #define RMW_THREAD_LOCAL _Thread_local
+  #else
+    #error "Unknown Apple platform"
+  #endif
 #else
-  #define RMW_THREAD_LOCAL __thread
+// Some other non-Windows, non-cygwin, non-apple OS
+  #define RMW_THREAD_LOCAL _Thread_local
 #endif
 
 #define RMW_STRINGIFY_IMPL(x) #x
