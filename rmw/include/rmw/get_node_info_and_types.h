d2f046a4342f91db40ab3a4a2d263e8a84d0c0aa
Ross Desmond <rddesmon@amazon.com>
2018-10-30 20:41:55 +0000

    Add namespace to graph functions

--- rmw/include/rmw/get_node_info_and_types.h
+++ rmw/include/rmw/get_node_info_and_types.h
@@ -1,4 +1,5 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -41,6 +42,8 @@ extern "C"
  * \param[in] node the handle to the node being used to query the ROS graph
  * \param[in] allocator allocator to be used when allocating space for strings
  * \param[in] node_name the name of the node to get information for
+ * \param[in] node_namespace the namespace of the node to get information for
+ * \param[in] no_demangle if true, list all topics without any demangling
  * \param[out] topic_names_and_types list of topic names and their types the node_name is subscribed to
  * \return `RMW_RET_OK` if the query was successful, or
  * \return `RMW_RET_INVALID_ARGUMENT` if the node is invalid, or
@@ -49,12 +52,13 @@ extern "C"
  * \return `RMW_RET_ERROR` if an unspecified error occurs.
  */
 RMW_PUBLIC
-        RMW_WARN_UNUSED
+RMW_WARN_UNUSED
 rmw_ret_t
 rmw_get_subscriber_names_and_types_by_node(
         const rmw_node_t * node,
         rcutils_allocator_t * allocator,
         const char * node_name,
+        const char * node_namespace,
         bool demangle,
         rmw_names_and_types_t * topics_names_and_types);
 
@@ -74,6 +78,8 @@ rmw_get_subscriber_names_and_types_by_node(
  * \param[in] node the handle to the node being used to query the ROS graph
  * \param[in] allocator allocator to be used when allocating space for strings
  * \param[in] node_name the name of the node to get information for
+ * \param[in] node_namespace the namespace of the node to get information for
+ * \param[in] no_demangle if true, list all topics without any demangling
  * \param[out] topic_names_and_types list of topic names and their types the node_name is publishing
  * \return `RMW_RET_OK` if the query was successful, or
  * \return `RMW_RET_INVALID_ARGUMENT` if the node is invalid, or
@@ -82,12 +88,13 @@ rmw_get_subscriber_names_and_types_by_node(
  * \return `RMW_RET_ERROR` if an unspecified error occurs.
  */
 RMW_PUBLIC
-        RMW_WARN_UNUSED
+RMW_WARN_UNUSED
 rmw_ret_t
 rmw_get_publisher_names_and_types_by_node(
         const rmw_node_t * node,
         rcutils_allocator_t * allocator,
         const char * node_name,
+        const char * node_namespace,
         bool demangle,
         rmw_names_and_types_t * topic_names_and_types);
 
@@ -107,6 +114,7 @@ rmw_get_publisher_names_and_types_by_node(
  * \param[in] node the handle to the node being used to query the ROS graph
  * \param[in] allocator allocator to be used when allocating space for strings
  * \param[in] node_name the name of the node to get information for
+ * \param[in] node_namespace the namespace of the node to get information for
  * \param[out] topic_names_and_types list of topic names and their types the node_name has created a service for
  * \return `RMW_RET_OK` if the query was successful, or
  * \return `RMW_RET_INVALID_ARGUMENT` if the node is invalid, or
@@ -121,6 +129,7 @@ rmw_get_service_names_and_types_by_node(
         const rmw_node_t * node,
         rcutils_allocator_t * allocator,
         const char * node_name,
+        const char * node_namespace,
         rmw_names_and_types_t * service_names_and_types);
 
 #ifdef __cplusplus
