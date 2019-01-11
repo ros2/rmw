132f3066f7026f9bd7b48741cf2f775b924c3857
Mike Lautman <mikeblautman@gmail.com>
2019-01-11 09:02:04 -0700

    minor notes explaining rmw_qos_profile_t (#156)
    * minor notes explaining rmw_qos_profile_t
    
    * Doxygen
    
    * Doxygen
    
    * fixup
    
    * Update rmw/include/rmw/types.h
    
    * using triple slash to match style

--- rmw/include/rmw/types.h
+++ rmw/include/rmw/types.h
@@ -192,9 +192,11 @@ enum RMW_PUBLIC_TYPE rmw_qos_durability_policy_t
   RMW_QOS_POLICY_DURABILITY_VOLATILE
 };
 
+/// ROS MiddleWare quality of service profile.
 typedef struct RMW_PUBLIC_TYPE rmw_qos_profile_t
 {
   enum rmw_qos_history_policy_t history;
+  /// Size of the message queue.
   size_t depth;
   enum rmw_qos_reliability_policy_t reliability;
   enum rmw_qos_durability_policy_t durability;
