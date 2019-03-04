d2b076d19b879fec6e24de8547f5cc8d8412e1f0
Ross Desmond <rddesmon@amazon.com>
2019-03-04 22:54:17 +0000

    Add rmw take_event interface
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    Signed-off-by: Burek <burekn@f45c89c6612b.ant.amazon.com>

--- rmw/include/rmw/rmw.h
+++ rmw/include/rmw/rmw.h
@@ -444,6 +444,23 @@ rmw_take_serialized_message_with_info(
   bool * taken,
   rmw_message_info_t * message_info);
 
+/*
+ * Take an event from the event handle.
+ *
+ * \param event subscription object to take from
+ * \param taken boolean flag indicating if an event was taken or not
+ * \return `RMW_RET_OK` if successful, or
+ * \return `RMW_RET_BAD_ALLOC` if memory allocation failed, or
+ * \return `RMW_RET_ERROR` if an unexpected error occurs.
+ */
+RMW_PUBLIC
+RMW_WARN_UNUSED
+rmw_ret_t
+rmw_take_event(
+  const rmw_event_t * event_handle,
+  void * event,
+  bool * taken);
+
 RMW_PUBLIC
 RMW_WARN_UNUSED
 rmw_client_t *
