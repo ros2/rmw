4f74f6e8c50841ff82f6fafdc4b930892cd8b3f2
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2020-02-28 15:14:51 -0300

    Delete const qualifier
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- rmw/src/security_options.c
+++ rmw/src/security_options.c
@@ -64,7 +64,7 @@ rmw_security_options_set_root_path(
   RMW_CHECK_ARGUMENT_FOR_NULL(security_root_path, RMW_RET_INVALID_ARGUMENT);
   RCUTILS_CHECK_ALLOCATOR(allocator, return RMW_RET_INVALID_ARGUMENT);
   RMW_CHECK_ARGUMENT_FOR_NULL(security_options, RMW_RET_INVALID_ARGUMENT);
-  const char * old_security_root_path = security_options->security_root_path;
+  char * old_security_root_path = security_options->security_root_path;
   security_options->security_root_path = rcutils_strdup(security_root_path, *allocator);
   if (!security_options->security_root_path) {
     security_options->security_root_path = old_security_root_path;
