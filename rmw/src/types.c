9677d2bfa919b73b419fe4a2aaafec4cce2d1718
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2022-03-10 16:26:47 -0300

    Zero initialize rmw_message_info_t
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- rmw/src/types.c
+++ rmw/src/types.c
@@ -19,9 +19,6 @@ RMW_WARN_UNUSED
 rmw_message_info_t
 rmw_get_zero_initialized_message_info(void)
 {
-  rmw_message_info_t zero_initialized_message_info = {
-    0, 0, RMW_MESSAGE_INFO_SEQUENCE_NUMBER_UNSUPPORTED,
-    RMW_MESSAGE_INFO_SEQUENCE_NUMBER_UNSUPPORTED, {NULL, {0}}, false
-  };
+  rmw_message_info_t zero_initialized_message_info = {0};
   return zero_initialized_message_info;
 }
