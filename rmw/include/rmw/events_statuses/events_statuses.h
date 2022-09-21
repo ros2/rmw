0cf566ed505ba673495bc751a4a85b7f3c880587
Barry Xu <barry.xu@sony.com>
2022-09-21 16:35:25 +0800

    Add connect & disconnect event
    Signed-off-by: Barry Xu <barry.xu@sony.com>

--- rmw/include/rmw/events_statuses/events_statuses.h
+++ rmw/include/rmw/events_statuses/events_statuses.h
@@ -22,5 +22,6 @@
 #include "rmw/events_statuses/message_lost.h"
 #include "rmw/events_statuses/offered_deadline_missed.h"
 #include "rmw/events_statuses/requested_deadline_missed.h"
+#include "rmw/events_statuses/matched.h"
 
 #endif  // RMW__EVENTS_STATUSES__EVENTS_STATUSES_H_
---
+++ rmw/include/rmw/events_statuses/matched.h
@@ -0,0 +1,60 @@
+// Copyright 2022 Sony Group Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef RMW__EVENTS_STATUSES__MATCHED_H_
+#define RMW__EVENTS_STATUSES__MATCHED_H_
+
+#include <stddef.h>
+
+#include "rmw/visibility_control.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef struct RMW_PUBLIC_TYPE rmw_matched_status_s
+{
+  /**
+   * For publisher, total cumulative count the concerned publisher discovered a 'match' with
+   * a subscriber.
+   *
+   * For subscriber, total cumulative count the concerned subscriber discovered a 'match' with
+   * a publisher.
+   */
+  size_t total_count;
+
+  /**
+   * The change in total_count since the last time the status was read.
+   */
+  size_t total_count_change;
+
+  /**
+   * For publisher, the number of subscribers currently matched to the concerned publisher.
+   *
+   * For subscriber, the number of publishers currently matched to the concerned subscriber.
+   */
+  size_t current_count;
+
+  /**
+   * The change in current_count since the last time the status was read.
+   */
+  size_t current_count_change;
+} rmw_matched_status_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // RMW__EVENTS_STATUSES__MATCHED_H_
