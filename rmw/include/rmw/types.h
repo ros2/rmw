ab17676be8f3daa863063ab7cb838a294b65196c
Tomoya Fujita <Tomoya.Fujita@sony.com>
2025-12-16 08:53:23 +0900

    Remove default from switch with enum, so that compiler warns.
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- rmw/include/rmw/types.h
+++ rmw/include/rmw/types.h
@@ -461,10 +461,6 @@ typedef enum RMW_PUBLIC_TYPE rmw_qos_durability_policy_e
   RMW_QOS_POLICY_DURABILITY_BEST_AVAILABLE
 } rmw_qos_durability_policy_t;
 
-#define RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_NODE_DEPRECATED_MSG \
-  "RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_NODE is deprecated. " \
-  "Use RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_TOPIC if manually asserted liveliness is needed."
-
 #ifndef _MSC_VER
 # define RMW_DECLARE_DEPRECATED(name, msg) name __attribute__((deprecated(msg)))
 #else
@@ -482,13 +478,6 @@ typedef enum RMW_PUBLIC_TYPE rmw_qos_liveliness_policy_e
 
   /// The signal that establishes a Topic is alive comes from the ROS rmw layer.
   RMW_QOS_POLICY_LIVELINESS_AUTOMATIC = 1,
-
-  /// Explicitly asserting node liveliness is required in this case.
-  /// This option is deprecated, use RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_TOPIC if your application
-  /// requires to assert liveliness manually.
-  RMW_DECLARE_DEPRECATED(
-    RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_NODE,
-    RMW_QOS_POLICY_LIVELINESS_MANUAL_BY_NODE_DEPRECATED_MSG) = 2,
 
   /// The signal that establishes a Topic is alive is at the Topic level. Only publishing a message
   /// on the Topic or an explicit signal from the application to assert liveliness on the Topic
